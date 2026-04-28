109a25a7492c7a9b149b7d48fe2dab370d9d9dda
long-while <unknown.recxrd@gmail.com>
2026-04-27 20:53:53 -0400

    edit default visibility

--- app/controllers/settings/profiles_controller.rb
+++ app/controllers/settings/profiles_controller.rb
@@ -8,8 +8,10 @@ class Settings::ProfilesController < Settings::BaseController
   end
 
   def update
-    if UpdateAccountService.new.call(@account, account_attributes)
-      apply_private_account_settings!
+    toggle_changed = private_account_toggle_changed?
+
+    if UpdateAccountService.new.call(@account, account_attributes(toggle_changed))
+      apply_private_account_settings! if toggle_changed
       ActivityPub::UpdateDistributionWorker.perform_in(ActivityPub::UpdateDistributionWorker::DEBOUNCE_DELAY, @account.id)
       redirect_to settings_profile_path, notice: I18n.t('generic.changes_saved_msg')
     else
@@ -24,15 +26,13 @@ class Settings::ProfilesController < Settings::BaseController
     params.expect(account: [:display_name, :note, :avatar, :header, :bot, fields_attributes: [[:name, :value]]])
   end
 
-  def account_attributes
-    return account_params unless private_account_toggle_changed?
+  def account_attributes(toggle_changed)
+    return account_params unless toggle_changed
 
     account_params.merge(locked: private_account_enabled?, hide_collections: private_account_enabled?)
   end
 
   def apply_private_account_settings!
-    return unless private_account_toggle_changed?
-
     current_user.update!(settings_attributes: { default_privacy: private_account_enabled? ? 'private' : 'unlisted' })
   end
 
